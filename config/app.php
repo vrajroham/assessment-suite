1ce4e5461dcb104433651b53622d6c7f76e2468d
Madhuri Rahane <madhuri.rahane66@gmail.com>
2017-08-07 18:06:37 +0530

    created user roles

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
